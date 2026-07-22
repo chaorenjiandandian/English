7f679c5822b626937cf4edb152a8dfcb5f103436
chaorenjiandandian <54519173+chaorenjiandandian@users.noreply.github.com>
2026-07-22 18:25:47 +0800

    4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'english-buddy-v30';
+const CACHE = 'english-buddy-v38';
 const ASSETS = ['./', './index.html', './icon-180.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
