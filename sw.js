c484ba4040a323ebe0ab7d4380dca6927a5789c6
chaorenjiandandian <54519173+chaorenjiandandian@users.noreply.github.com>
2026-07-24 17:27:18 +0800

    v1.0.2
    修复多词连读的问题

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'english-buddy-v73';
+const CACHE = 'english-buddy-v74';
 const ASSETS = ['./', './index.html', './icon-180.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
